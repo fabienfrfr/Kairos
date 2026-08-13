4b6342e50b223e93f9beec2ffab0a0a1667c07de
fabienfrfr <fabien.furfaro@gmail.com>
2026-08-13 22:26:04 +0200

    fix: update version to 0.0.1

--- kairos/_version.py
+++ kairos/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.0.dev13'
+__version__ = version = '0.0.1.dev13'
 __version_tuple__ = version_tuple = (0, 0, 0, 'dev13')
 
 __commit_id__ = commit_id = 'g563c96347'
