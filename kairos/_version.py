db8c09d99c4ca99c8f4a503c8a9b7a90945bd042
fabienfrfr <fabien.furfaro@gmail.com>
2026-08-15 10:38:50 +0200

    v0.1.0

--- kairos/_version.py
+++ kairos/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.1.dev28'
-__version_tuple__ = version_tuple = (0, 0, 1, 'dev28')
+__version__ = version = '0.1.0.dev28'
+__version_tuple__ = version_tuple = (0, 1, 0, 'dev28')
 
 __commit_id__ = commit_id = 'g0a4b5b686'
