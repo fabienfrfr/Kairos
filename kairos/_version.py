346f759bd1ed69a1f8945b1f32cafcac7f031054
fabienfrfr <fabien.furfaro@gmail.com>
2026-09-09 23:07:11 +0200

    chore: bump version to 0.4.0

--- kairos/_version.py
+++ kairos/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.3.2.dev18'
-__version_tuple__ = version_tuple = (0, 3, 2, 'dev18')
+__version__ = version = '0.4.1.dev46'
+__version_tuple__ = version_tuple = (0, 4, 1, 'dev46')
 
-__commit_id__ = commit_id = 'gdcfebc7bd'
+__commit_id__ = commit_id = 'g4412ea111'
